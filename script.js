5dff7e2c3e387e8750cd25a7486800fa8b3abd6f
Koréh Szilárd <106010278+korehszilard@users.noreply.github.com>
2023-05-17 19:34:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var autok = new Map();
+
+function Auto(gyarto, tipus, szin){
+    this.gyarto = gyarto;
+    this.tipus = tipus;
+    this.szin = szin;
+}
+
+document.getElementById("gomb").addEventListener("click", function(){
+    var rsz = document.getElementById("rendszam").value;
+    var gy = document.getElementById("gyarto").value;
+    var t = document.getElementById("tipus").value;
+    var sz = document.getElementById("szin").value;
+
+    var aktAuto = new Auto(gy, t, sz);
+
+    autok.set(rsz, aktAuto);
+    //var a1 = autok.get("ABC-123"); //Auto tipusu objektum
+
+    //console.log(autok);
+}, false);
+
+document.getElementById("keresesGomb").addEventListener("click", function(){
+    var keresettRendszam = document.getElementById("keresettRendszam").value;
+    var h3 = document.getElementById("eredmeny");
+
+    if(autok.has(keresettRendszam)){
+        var keresettAuto = autok.get(keresettRendszam);
+        h3.innerHTML = "Gyarto: " + keresettAuto.gyarto + ", tipus: " + keresettAuto.tipus + ", szin: " + keresettAuto.szin;
+    }
+    else{
+        h3.innerHTML = "A megadott rendszamhoz nem tartozik jarmu a rendszerben!";
+    }
+}, false);
